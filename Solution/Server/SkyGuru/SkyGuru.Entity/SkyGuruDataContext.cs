5a1ae053fed8211eeab1cdbf92033a3a4ec678e3
Serhan AKIN <serhanakin.py@gmail.com>
2023-11-15 23:52:46 +0300

    Data context has been created.

--- Solution/Server/SkyGuru/SkyGuru.Entity/SkyGuruDataContext.cs
+++ Solution/Server/SkyGuru/SkyGuru.Entity/SkyGuruDataContext.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace SkyGuru.Entity
 {
-    public class SkyGuruDataContext
+    public class SkyGuruDataContext : DbContext
     {
+        public SkyGuruDataContext() : base()
+        {
+
+        }
+
+        public SkyGuruDataContext(DbContextOptions<DbContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
     }
 }
