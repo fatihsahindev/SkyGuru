5496ef3cac73be11347beaf5ad799da7824342e4
Serhan AKIN <serhanakin.py@gmail.com>
2023-11-17 00:27:20 +0300

    WeatherController and service has been added.

---
+++ Solution/Server/SkyGuru/SkyGuru.Api/Controllers/WeatherController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenMeteo;
+using SkyGuru.Entity;
+
+namespace SkyGuru.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly SkyGuruDataContext _context;
+
+        public WeatherController(SkyGuruDataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("GetWeatherForecast")]
+        public ActionResult GetWeatherForecast(string city)
+        {
+            try
+            {
+                OpenMeteoClient client = new OpenMeteoClient();
+
+                WeatherForecast weatherData = client.QueryAsync(city).GetAwaiter().GetResult();
+
+                if (weatherData == null)
+                {
+                    return NotFound("Weather data not found for the given city.");
+                }
+
+                var currentWeather = weatherData.Current;
+
+                return Ok(currentWeather);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+
+    }
+}
--- Solution/Server/SkyGuru/SkyGuru.Api/Program.cs
+++ Solution/Server/SkyGuru/SkyGuru.Api/Program.cs
@@ -1,11 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using SkyGuru.Entity;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//VeriTabanı Baglantisi
+builder.Services.AddDbContext<SkyGuruDataContext>(x => x.UseSqlServer("Server=myServerAddress;Database=myDataBase;Trusted_Connection=True;"));
 
 var app = builder.Build();
 
@@ -16,10 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
