b2271d75c6bef384e4d2b45e921aa187099d96a8
Serhan AKIN <serhanakin.py@gmail.com>
2023-11-18 11:37:10 +0300

    weatherforecast displayed on client.

--- Solution/Client/src/app/app.module.ts
+++ Solution/Client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BarComponent } from './bar/bar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { BarComponent } from './bar/bar.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- Solution/Client/src/app/home/home.component.ts
+++ Solution/Client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../providers/weather.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  // weatherIcon!: string;
+  subscribes: Subscription[] = [];
+  weatherForecast: any;
+  weatherIcon: string | undefined;
   // weatherCode!: number;
 
   unit: { type: string; selected: boolean }[] = [
@@ -15,12 +18,12 @@ export class HomeComponent implements OnInit {
     { type: 'Fahrenheit', selected: false },
   ];
 
-  info = {
-    temperature: '12',
-    location: 'Harrisonburg',
-    date: this.getDate(),
-    weather: 'Rainy',
-  };
+  // info = {
+  //   temperature: '12',
+  //   location: 'Harrisonburg',
+  //   date: this.getDate(),
+  //   weather: 'Rainy',
+  // };
 
   detail = {
     cloudy: 86,
@@ -29,10 +32,11 @@ export class HomeComponent implements OnInit {
     weather: 7,
   };
 
-  constructor() {}
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.getDate();
+    this.getWeatherForecast();
   }
 
   toggleSelection(unit: { type: string; selected: boolean }): void {
@@ -42,99 +46,123 @@ export class HomeComponent implements OnInit {
 
   getDate() {
     let date;
-    let options: Intl.DateTimeFormatOptions = { weekday: 'long', hour: 'numeric', minute: 'numeric', hour12: true, };
+    let options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    };
     date = new Date().toLocaleString('en-US', options);
     return date;
   }
 
-  // getWeatherStatus(weatherCode: number) {
-  //   switch (weatherCode) {
-  //     case 0:
-  //       this.weatherIcon = 'sun';
-  //       break;
-  //     case 1:
-  //       this.weatherIcon = 'cloud';
-  //       break;
-  //     case 2:
-  //       this.weatherIcon = 'cloud';
-  //       break;
-  //     case 3:
-  //       this.weatherIcon = 'cloud';
-  //       break;
-  //     case 45:
-  //       this.weatherIcon = 'cloud-drizzle';
-  //       break;
-  //     case 48:
-  //       this.weatherIcon = 'cloud-drizzle';
-  //       break;
-  //     case 51:
-  //       this.weatherIcon = 'cloud-drizzle';
-  //       break;
-  //     case 53:
-  //       this.weatherIcon = 'cloud-drizzle';
-  //       break;
-  //     case 55:
-  //       this.weatherIcon = 'cloud-drizzle';
-  //       break;
-  //     case 56:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 57:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 61:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 63:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 65:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 66:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 67:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 71:
-  //       this.weatherIcon = 'cloud-snow';
-  //       break;
-  //     case 73:
-  //       this.weatherIcon = 'cloud-snow';
-  //       break;
-  //     case 75:
-  //       this.weatherIcon = 'cloud-snow';
-  //       break;
-  //     case 77:
-  //       this.weatherIcon = 'cloud-snow';
-  //       break;
-  //     case 80:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 81:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 82:
-  //       this.weatherIcon = 'cloud-rain';
-  //       break;
-  //     case 85:
-  //       this.weatherIcon = 'cloud-snow';
-  //       break;
-  //     case 86:
-  //       this.weatherIcon = 'cloud-snow';
-  //       break;
-  //     case 95:
-  //       this.weatherIcon = 'wind';
-  //       break;
-  //     case 96:
-  //       this.weatherIcon = 'cloud-lightning';
-  //       break;
-  //     case 99:
-  //       this.weatherIcon = 'cloud-lightning';
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
+  getWeatherForecast() {
+    this.subscribes.push(
+      this.weatherService.getWeatherForecast('Bursa').subscribe(
+        (res) => {
+          this.weatherForecast = res;
+          this.getWeatherStatus(res.weathercode);
+          // console.log(this.weatherForecast);
+        },
+        (err) => {
+          console.error('Weather Service Error: ', err);
+        }
+      )
+    );
+  }
+
+  getTemp(temp: number) {
+    return Math.floor(temp);
+  }
+
+  getWeatherStatus(weatherCode: number) {
+    switch (weatherCode) {
+      case 0:
+        this.weatherIcon = 'sun';
+        break;
+      case 1:
+        this.weatherIcon = 'cloud';
+        break;
+      case 2:
+        this.weatherIcon = 'cloud';
+        break;
+      case 3:
+        this.weatherIcon = 'cloud';
+        break;
+      case 45:
+        this.weatherIcon = 'cloud-drizzle';
+        break;
+      case 48:
+        this.weatherIcon = 'cloud-drizzle';
+        break;
+      case 51:
+        this.weatherIcon = 'cloud-drizzle';
+        break;
+      case 53:
+        this.weatherIcon = 'cloud-drizzle';
+        break;
+      case 55:
+        this.weatherIcon = 'cloud-drizzle';
+        break;
+      case 56:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 57:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 61:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 63:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 65:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 66:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 67:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 71:
+        this.weatherIcon = 'cloud-snow';
+        break;
+      case 73:
+        this.weatherIcon = 'cloud-snow';
+        break;
+      case 75:
+        this.weatherIcon = 'cloud-snow';
+        break;
+      case 77:
+        this.weatherIcon = 'cloud-snow';
+        break;
+      case 80:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 81:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 82:
+        this.weatherIcon = 'cloud-rain';
+        break;
+      case 85:
+        this.weatherIcon = 'cloud-snow';
+        break;
+      case 86:
+        this.weatherIcon = 'cloud-snow';
+        break;
+      case 95:
+        this.weatherIcon = 'wind';
+        break;
+      case 96:
+        this.weatherIcon = 'cloud-lightning';
+        break;
+      case 99:
+        this.weatherIcon = 'cloud-lightning';
+        break;
+      default:
+        break;
+    }
+  }
 }
